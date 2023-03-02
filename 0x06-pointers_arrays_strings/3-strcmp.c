13da40c663abb7d0d10ed5c7d6525c6a664e76cf
OkoroDanielM <okorodaniel143@gmail.com>
2023-03-02 03:41:14 -0800

    UPD

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,7 +2,7 @@
 /**
  * _strcmp - function that compares two strings.
  * @s1: input value
- * @s1: input value
+ * @s2: input value
  *
  * Return: s1[i] - s2[i]
  */
@@ -12,7 +12,6 @@ int _strcmp(char *s1, char *s2)
 	int i;
 
 	i = 0;
-
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
 		if (s1[i] != s2[i])
